ea2448aaba14155be0e873a9454494ec89cbf2ed
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 05:56:20 -0500

    cal

--- calculator.py
+++ calculator.py
@@ -6,24 +6,24 @@ root.title("simple calculator")
 e = Entry(root,width=35,borderwidth=5)
 e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
 
-def button_add():
-    return
+def button_add(number):
+    # e.delete(0,END)
+    e.insert(0,number)
 
-button_1=Button(root,text='1',padx=40,pady=20,command=button_add)
-button_2=Button(root,text='2',padx=40,pady=20,command=button_add)
-button_3=Button(root,text='3',padx=40,pady=20,command=button_add)
-button_4=Button(root,text='4',padx=40,pady=20,command=button_add)
-button_5=Button(root,text='5',padx=40,pady=20,command=button_add)
-button_6=Button(root,text='6',padx=40,pady=20,command=button_add)
-button_7=Button(root,text='7',padx=40,pady=20,command=button_add)
-button_8=Button(root,text='8',padx=40,pady=20,command=button_add)
-button_9=Button(root,text='9',padx=40,pady=20,command=button_add)
-button_0=Button(root,text='0',padx=40,pady=20,command=button_add)
+button_1=Button(root,text='1',padx=40,pady=20,command=lambda:button_add(1))
+button_2=Button(root,text='2',padx=40,pady=20,command=lambda:button_add(2))
+button_3=Button(root,text='3',padx=40,pady=20,command=lambda:button_add(3))
+button_4=Button(root,text='4',padx=40,pady=20,command=lambda:button_add(4))
+button_5=Button(root,text='5',padx=40,pady=20,command=lambda:button_add(5))
+button_6=Button(root,text='6',padx=40,pady=20,command=lambda:button_add(6))
+button_7=Button(root,text='7',padx=40,pady=20,command=lambda:button_add(7))
+button_8=Button(root,text='8',padx=40,pady=20,command=lambda:button_add(8))
+button_9=Button(root,text='9',padx=40,pady=20,command=lambda:button_add(9))
+button_0=Button(root,text='0',padx=40,pady=20,command=lambda:button_add(0))
 
-button_add = Button(root,text="+",padx=39,pady=20,command=button_add)
-button_equal= Button(root,text="=",padx=91,pady=20,command=button_add)
-button_clear = Button(root,text="Clear",padx=79,pady=20,command=button_add)
-
+button_ads = Button(root,text="+",padx=39,pady=20,command=lambda:button_add(0))
+button_equal= Button(root,text="=",padx=91,pady=20,command=lambda:button_add(0))
+button_clear = Button(root,text="Clear",padx=79,pady=20,command=lambda:button_add(0))
 
 
 # put buttins inscreen
@@ -41,7 +41,7 @@ button_9.grid(row=1,column=2)
 
 button_0.grid(row=4,column=0)
 button_clear.grid(row=4,column=1,columnspan=2)
-button_add.grid(row=5,column=0)
+button_ads.grid(row=5,column=0)
 button_equal.grid(row=5,column=1,columnspan=2)
 
 
