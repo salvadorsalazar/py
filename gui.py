c8fe2c075a55377f03d661210fc2579304d49543
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 03:12:10 -0500

    1st gui

--- gui.py
+++ gui.py
@@ -3,10 +3,17 @@ from tkinter import *
 
 
 root = Tk()
+
 # creating label widget
-myLabel = Label(root,text="Hello World")
+myLabel1 = Label(root,text="Hello World")
+myLabel2 = Label(root,text="my name is NOONE,,,")
+
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=1)
+
+
 # sjwoing onto screen
-myLabel.pack()
+# myLabel.pack()
 
 
 root.mainloop()
