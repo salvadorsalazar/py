470e73165a89d7205f37f788ff7f50b3b5c3c558
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 03:32:33 -0500

    3

---
+++ button.py
@@ -0,0 +1,18 @@
+
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root,text="button clicked")
+    myLabel.pack()
+
+
+myButton = Button(root,text="click me",command=myClick)
+# myButton = Button(root,text="click me", padx=50 ,pady=50)
+
+myButton.pack()
+
+
+
+root.mainloop()
