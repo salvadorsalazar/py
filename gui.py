04e2b8b6ef8c00b6d43055d348696bc82d18bc83
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 03:18:49 -0500

    2

--- gui.py
+++ gui.py
@@ -5,14 +5,14 @@ from tkinter import *
 root = Tk()
 
 # creating label widget
-myLabel1 = Label(root,text="Hello World")
-myLabel2 = Label(root,text="my name is s ")
+myLabel1 = Label(root,text="Hello World").grid(row=0,column=0)
+myLabel2 = Label(root,text="my name is s ").grid(row=1,column=0)
 
-myLabel1.grid(row=0,column=0)
-myLabel2.grid(row=1,column=1)
+# myLabel1.grid(row=0,column=0)
+# myLabel2.grid(row=1,column=1)
 
 
-# sjwoing onto screen
+# shoing onto screen
 # myLabel.pack()
 
 
