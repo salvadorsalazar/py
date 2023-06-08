57dce69e682cb7bd57888a44704888c19a490453
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 02:50:58 -0500

    test

--- demo.py
+++ demo.py
@@ -1,1 +1,17 @@
-print("first")
+# age = 45
+# price=19.95
+# first_name='Sal'
+
+# print("first")
+# print(age)
+# # is_online =True
+
+# **************************************
+print('=============================')
+print('=============================')
+
+name = input("what is your name? ")
+age = input("what is your age ")
+
+print("Hello "+ name)
+print("your age is " +age)
