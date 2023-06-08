93244c62cf55e82b22606602f6a767c73874c9b1
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-06-08 04:08:41 -0500

    buttons

--- entry.py
+++ entry.py
@@ -4,13 +4,17 @@ from tkinter import *
 root = Tk()
 e = Entry(root,width=50, bg="white",fg="black")
 e.pack()
+# e,get()
+
+e.insert(0,"enter your name: ")
 
 def myClick():
-    myLabel = Label(root,text="button clicked")
+    hello = "hello "+ e.get()
+    myLabel = Label(root,text=hello)
     myLabel.pack()
 
 
-myButton = Button(root,text="click me",command=myClick,bg="blue")
+myButton = Button(root,text="enter your name",command=myClick,bg="blue")
 # myButton = Button(root,text="click me", padx=50 ,pady=50)
 
 myButton.pack()
